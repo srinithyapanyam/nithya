bac5569d129d23f3991f86eb8a107b059a590e28
srinithyapanyam <60380139+srinithyapanyam@users.noreply.github.com>
2020-02-10 21:51:41 +0530

    Add files via upload

--- module-info.java
+++ module-info.java
@@ -1,403 +1,224 @@
-module Gift1.java {
-	package com.epam.GiftAnalysis;
-	import java.util.*;
-	public class Gift {
-		static Scanner input = new Scanner(System.in);
-		static ArrayList<Chocolates> chocolates = new ArrayList<Chocolates>();
-	    static ArrayList<Sweets> sweets = new ArrayList<Sweets>();
-
-	    public static void main(String[] args) {
-	        inputChocolates();
-	        inputSweets();
-	        System.out.println("Total Weight of the Gift: " + totalWeight());
-	        System.out.println();
-	        System.out.println("Total Price of the Gift: " + totalPrice());
-	        System.out.println();
-	        System.out.println("Details of Gift after sorting by: ");
-	        System.out.println();
-	        System.out.println("1. Weight");
-	        sortedByWeight();
-	        System.out.println();
-	        System.out.println("2. Price");
-	        sortedByPrice();
-	    }
-	    private static void inputChocolates() {
-	        System.out.println("Enter No. of Chocolates in Gift");
-	        int numberOfChocolates = input.nextInt();
-	        for (int itr = 0; itr < numberOfChocolates; itr++) {
-	            System.out.println("Enter Weight of the Chocolate:");
-	            int weight = input.nextInt();
-	            System.out.println("Enter Price of the Chocolate:");
-	            int price = input.nextInt();
-	            System.out.println("Select from below options: ");
-	            System.out.println("1. DairyMilk \n2. KitKat");
-	            int typeOfChocolate = input.nextInt();
-	            boolean flag = false;
-	            switch (typeOfChocolate) {
-	            case 1:
-	                for (Chocolates item : chocolates) 
-	                {
-	                    if (item.getName() == "DairyMilk") 
-	                    {
-	                        flag = true;
-	                        item.addWeight(weight);
-	                        item.addPrice(price);
-	                    }
-	                }
-	                if (!flag) 
-	                {
-	                	DairyMilk dm = new DairyMilk(weight, price);
-	                    chocolates.add(dm);
-	                }
-	                break;
-	            case 2:
-	                for (Chocolates item : chocolates) 
-	                {
-	                    if (item.getName() == "KitKat") 
-	                    {
-	                        flag = true;
-	                        item.addWeight(weight);
-	                        item.addPrice(price);
-	                    }
-	                }
-	                if (!flag) 
-	                {
-	                	KitKat k = new KitKat(weight, price);
-	                    chocolates.add(k);
-	                }
-	                break;
-	            default:
-	                System.out.println("Please Enter a valid Option");
-	                itr--;
-	                break;
-	            }
-	        }
-
-	    }
-
-	    private static void inputSweets() {
-	        System.out.println("Enter No. of Sweets in Gift");
-	        int numberOfSweets = input.nextInt();
-	        for (int itr = 0; itr < numberOfSweets; itr++) 
-	        {
-	            System.out.println("Enter Weight of the Sweets:");
-	            int weight = input.nextInt();
-	            System.out.println("Enter Price of the Sweets:");
-	            int price = input.nextInt();
-	            System.out.println("Select from below options: ");
-	            System.out.println("1. KajuBarfi \n2. GulabJamun ");
-	            int typeOfSweets = input.nextInt();
-	            boolean flag = false;
-	            switch (typeOfSweets) 
-	            {
-	            case 1:
-	                for (Sweets item : sweets) 
-	                {
-	                    if (item.getName() == "KajuBarfi") 
-	                    {
-	                        flag = true;
-	                        item.addWeight(weight);
-	                        item.addPrice(price);
-	                    }
-	                }
-	                if (!flag) {
-	                    KajuBarfi kajuBarfi = new KajuBarfi(weight, price);
-	                    sweets.add(kajuBarfi);
-	                }
-	                break;
-	            case 2:
-	                for (Sweets item : sweets) 
-	                {
-	                    if (item.getName() == "GulabJamun") 
-	                    {
-	                        flag = true;
-	                        item.addWeight(weight);
-	                        item.addPrice(price);
-	                    }
-	                }
-	                if (!flag) 
-	                {
-	                    GulabJamun gulabJamun = new GulabJamun(weight, price);
-	                    sweets.add(gulabJamun);
-	                }
-	                break;
-	            
-	            default:
-	                System.out.println("Please Enter a valid Option");
-	                itr--;
-	                break;
-	            }
-	        }
-	    }
-
-	    public static int totalWeight() 
-	    {
-	        int total1 = 0;
-	        for (Chocolates item : chocolates) 
-	        {
-	            total1 += item.getWeight();
-	        }
-	        for (Sweets item : sweets) 
-	        {
-	            total1 += item.getWeight();
-	        }
-
-	        return total1;
-
-	    }
-	    public static int totalPrice() 
-	    {
-	        int total2 = 0;
-	        for (Chocolates item : chocolates) 
-	        {
-	            total2 += item.getPrice();
-	        }
-	        for (Sweets item : sweets) 
-	        {
-	            total2 += item.getPrice();
-	        }
-
-	        return total2;
-	    }
-	    public static void sortedByWeight() {
-	        Comparator<Chocolates> chocolateWeightComparator = (Chocolates c1,Chocolates c2) -> ((Integer)c1.getWeight().compareTo(c2.getWeight()));
-	        Collections.sort(chocolates, chocolateWeightComparator);
-
-	        for (Chocolates i : chocolates) {
-	            System.out.println("Name of Chocolate: " + i.getName());
-	            System.out.println("Weight of Chocolate: " + i.getWeight());
-	            System.out.println("Price of Chocolate: " + i.getPrice());
-	            System.out.println();
-	        }
-
-	        Comparator<Sweets> sweetsWeightComparator = (Sweets c1,Sweets c2) -> ((Integer) c1.getWeight().compareTo(c2.getWeight()));
-	                
-
-	        Collections.sort(sweets, sweetsWeightComparator);
-
-	        for (Sweets i : sweets) {
-	            System.out.println("Name of Sweet: " + i.getName());
-	            System.out.println("Weight of Sweet: " + i.getWeight());
-	            System.out.println("Price of Sweet: " + i.getPrice());
-	            System.out.println();
-	        }
-	    }
-
-	    public static void sortedByPrice() {
-	        Comparator<Chocolates> chocolatePriceComparator = (Chocolates c1,Chocolates c2) -> ((Integer) c1.getPrice().compareTo(c2.getPrice()));
-	                
-
-	        Collections.sort(chocolates, chocolatePriceComparator);
-
-	        for (Chocolates i : chocolates) {
-	            System.out.println("Name of Chocolate: " + i.getName());
-	            System.out.println("Weight of Chocolate: " + i.getWeight());
-	            System.out.println("Price of Chocolate: " + i.getWeight());
-	            System.out.println();
-	        }
-
-	        Comparator<Sweets> sweetsPriceComparator = (Sweets c1,Sweets c2) -> ((Integer) c1.getPrice().compareTo(c2.getPrice()));
-	                
-
-	        Collections.sort(sweets, sweetsPriceComparator);
-
-	        for (Sweets i : sweets) {
-	            System.out.println("Name of Sweet: " + i.getName());
-	            System.out.println("Weight of Sweet: " + i.getWeight());
-	            System.out.println("Price of Sweet: " + i.getWeight());
-	            System.out.println();
-	        }
-	    }
-	}
-	//////////////////////////////////////////////////////////////////////////////
-	Chocolates.java
-	///////////////////////////////////////////////////////////////////////////
-	package com.epam.GiftAnalysis;
-
-	public class Chocolates implements NewYearGift{
-		private Integer weight, price;
-	    private String name;
-
-	    public Chocolates(String name, Integer weight, Integer price) 
-	    {
-	        this.name = name;
-	        this.price = price;
-	        this.weight = weight;
-	    }
-
-	    @Override
-	    public Integer getPrice() 
-	    {
-	    
-	        return this.price;
-	    }
-
-	    @Override
-	    public Integer getWeight() {
-
-	        return this.weight;
-	    }
-
-	    public String getName() {
-	        return this.name;
-	    }
-
-	    public void addWeight(Integer weight) {
-	        this.weight += weight;
-	    }
-
-	    public void addPrice(Integer price) {
-	        this.price += price;
-	    }
-
-	}
-	//////////////////////////////////////////////////////////////////
-	DairyMilk.java
-	/////////////////////////////////////////////////////////////
-	package com.epam.GiftAnalysis;
-
-	public class DairyMilk extends Chocolates{
-		public DairyMilk(Integer weight, Integer price) {
-	        super("DairyMilk", weight, price);
-	}
-	}
-	///////////////////////////////////////////////////////
-	GulabJamun.java
-	//////////////////////////////////////////////////
-	package com.epam.GiftAnalysis;
-
-	public class GulabJamun extends Sweets{
-		public GulabJamun(Integer weight, Integer price) {
-	        super("GulabJamun", weight, price);
-	    }
-
-	    @Override
-	    public String getName() {
-	        // TODO Auto-generated method stub
-	        return super.name;
-	    }
-
-	    @Override
-	    public Integer getPrice() {
-	        // TODO Auto-generated method stub
-	        return super.price;
-	    }
-
-	    @Override
-	    public Integer getWeight() {
-	        // TODO Auto-generated method stub
-	        return super.weight;
-	    }
-
-	    @Override
-	    public void addWeight(Integer weight) {
-	        super.weight += weight;
-	    }
-
-	    @Override
-	    public void addPrice(Integer price) {
-	        super.price += price;
-	    }
-	}
-	///////////////////////////////////////////////////////////////
-	KajuBarfi.java
-	//////////////////////////////////////////////////////////////////
-	package com.epam.GiftAnalysis;
-
-	public class KajuBarfi extends Sweets{
-		public KajuBarfi(Integer weight, Integer price) {
-	        super("KajuBarfi", weight, price);
-	    }
-
-	    @Override
-	    public String getName() {
-	        // TODO Auto-generated method stub
-	        return super.name;
-	    }
-
-	    @Override
-	    public Integer getPrice() {
-	        // TODO Auto-generated method stub
-	        return super.price;
-	    }
-
-	    @Override
-	    public Integer getWeight() {
-	        // TODO Auto-generated method stub
-	        return super.weight;
-	    }
-
-	    @Override
-	    public void addWeight(Integer weight) {
-	        super.weight += weight;
-	    }
-
-	    @Override
-	    public void addPrice(Integer price) {
-	        super.price += price;
-	    }
-	}
-	//////////////////////////////////////////////////////////////////////////
-	KitKat.java
-	//////////////////////////////////////////////////////////////
-	package com.epam.GiftAnalysis;
-
-	public class KitKat extends Chocolates{
-		public KitKat(Integer weight, Integer price){
-	        super("KitKat", weight, price);
-	}
-	}
-	////////////////////////////////////////////////////////////////
-	NewYearGift.java
-	////////////////////////////////////////////////////////////////
-	package com.epam.GiftAnalysis;
-
-	public interface NewYearGift {
-		public abstract Integer getWeight();
-
-	    public abstract Integer getPrice();
-	}
-	/////////////////////////////////////////////////////////////////////
-	Sweets.java
-	//////////////////////////////////////////////////////////////
-	package com.epam.GiftAnalysis;
-
-	public class Sweets implements NewYearGift {
-		protected Integer weight, price;
-	    protected String name;
-
-	    public Sweets(String name, Integer weight, Integer price) {
-	        this.name = name;
-	        this.price = price;
-	        this.weight = weight;
-	    }
-
-	    @Override
-	    public Integer getPrice() 
-	    {
-	    
-	        return this.price;
-	    }
-
-	    @Override
-	    public Integer getWeight() {
-
-	        return this.weight;
-	    }
-
-	    public String getName() {
-	        return this.name;
-	    }
-
-	    public void addWeight(Integer weight) {
-	        this.weight += weight;
-	    }
-
-	    public void addPrice(Integer price) {
-	        this.price += price;
-	    }
-
-	}
-	/////////////////////////////////////////////////////////////////
-
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import java.awt.event.*;
+public class calculator implements ActionListener
+{
+    int c,n;
+    String s1,s2,s3,s4,s5;
+    JFrame f;
+    JButton b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17;
+    JPanel p;
+  JTextField tf;
+    GridLayout g;
+    calculator()
+    {
+        f = new JFrame("calulator");
+        p = new JPanel();
+        f.setLayout(new FlowLayout());
+        b1 = new JButton("0");
+        b1.addActionListener(this);
+        b2 = new JButton("1");
+        b2.addActionListener(this);
+        b3 = new JButton("2");
+        b3.addActionListener(this);
+        b4 = new JButton("3");
+        b4.addActionListener(this);
+        b5 = new JButton("4");
+        b5.addActionListener(this);
+        b6 = new JButton("5");
+        b6.addActionListener(this);
+        b7 = new JButton("6");
+        b7.addActionListener(this);
+        b8 = new JButton("7");
+        b8.addActionListener(this);
+        b9 = new JButton("8");
+        b9.addActionListener(this);
+        b10 = new JButton("9");
+        b10.addActionListener(this);
+        b11 = new JButton("+");
+        b11.addActionListener(this);
+        b12 = new JButton("-");
+        b12.addActionListener(this);
+        b13 = new JButton("*");
+        b13.addActionListener(this);
+        b14 = new JButton("%");
+        b14.addActionListener(this);
+        b15 = new JButton("/");
+        b15.addActionListener(this);
+        b16 = new JButton("=");
+        b16.addActionListener(this);
+        b17 = new JButton("c");
+        b17.addActionListener(this);
+        tf = new JTextField(20);
+        f.add(tf);
+        g = new GridLayout(4,4,10,20);
+        p.setLayout(g);
+        p.add(b1);p.add(b2);p.add(b3);p.add(b4);p.add(b5);p.add(b6);p.add(b7);p.add(b8);p.add(b9);
+        p.add(b10);p.add(b11);p.add(b12);p.add(b13);p.add(b14);p.add(b15);p.add(b16);p.add(b17);
+        f.add(p);
+        f.setSize(300,300);
+        f.setVisible(true);
+    }
+    public void actionPerformed(ActionEvent e)
+    {
+        if(e.getSource()==b1)
+        {
+            s3 = tf.getText();
+            s4 = "0";
+            s5 = s3+s4;
+            tf.setText(s5);
+        }
+        if(e.getSource()==b2)
+        {
+            s3 = tf.getText();
+            s4 = "1";
+            s5 = s3+s4;
+            tf.setText(s5);
+        }
+        if(e.getSource()==b3)
+        {
+            s3 = tf.getText();
+            s4 = "2";
+            s5 = s3+s4;
+            tf.setText(s5);
+        }if(e.getSource()==b4)
+        {
+            s3 = tf.getText();
+            s4 = "3";
+            s5 = s3+s4;
+            tf.setText(s5);
+        }
+        if(e.getSource()==b5)
+        {
+            s3 = tf.getText();
+            s4 = "4";
+            s5 = s3+s4;
+            tf.setText(s5);
+        }
+        if(e.getSource()==b6)
+        {
+            s3 = tf.getText();
+            s4 = "5";
+            s5 = s3+s4;
+            tf.setText(s5);
+        }
+        if(e.getSource()==b7)
+        {
+            s3 = tf.getText();
+            s4 = "6";
+            s5 = s3+s4;
+            tf.setText(s5);
+        }
+        if(e.getSource()==b8)
+        {
+            s3 = tf.getText();
+            s4 = "7";
+            s5 = s3+s4;
+            tf.setText(s5);
+        }
+        if(e.getSource()==b9)
+        {
+            s3 = tf.getText();
+            s4 = "8";
+            s5 = s3+s4;
+            tf.setText(s5);
+        }
+        if(e.getSource()==b10)
+        {
+            s3 = tf.getText();
+            s4 = "9";
+            s5 = s3+s4;
+            tf.setText(s5);
+        }
+        if(e.getSource()==b11)
+        {
+            s1 = tf.getText();
+            tf.setText("");
+            c=1;
+ 
+        }
+        if(e.getSource()==b12)
+        {
+            s1 = tf.getText();
+            tf.setText("");
+            c=2;
+ 
+        }
+        if(e.getSource()==b13)
+        {
+            s1 = tf.getText();
+            tf.setText("");
+            c=3;
+ 
+        }
+        if(e.getSource()==b14)
+        {
+            s1 = tf.getText();
+            tf.setText("");
+            c=4;
+ 
+        }
+        if(e.getSource()==b15)
+        {
+            s1 = tf.getText();
+            tf.setText("");
+            c=5;
+ 
+        
+        if(e.getSource()==b16)
+        {
+            s2 = tf.getText();
+            if(c==1)
+            {
+                n = Integer.parseInt(s1)+Integer.parseInt(s2);
+                tf.setText(String.valueOf(n));
+            }
+            else
+            if(c==2)
+            {
+                n = Integer.parseInt(s1)-Integer.parseInt(s2);
+                tf.setText(String.valueOf(n));
+            }
+            else
+            if(c==3)
+            {
+                n = Integer.parseInt(s1)*Integer.parseInt(s2);
+                tf.setText(String.valueOf(n));
+            }
+            if(c==4)
+            {
+                try
+                {
+                    int p=Integer.parseInt(s2);
+                    if(p!=0)
+                    {
+                                        n = Integer.parseInt(s1)/Integer.parseInt(s2);
+                    tf.setText(String.valueOf(n));
+                     }
+                     else
+                        tf.setText("infinite");
+ 
+                }
+                catch(Exception i){}
+            }
+            if(c==5)
+            {
+                n = Integer.parseInt(s1)%Integer.parseInt(s2);
+                tf.setText(String.valueOf(n));
+            }
+        }
+        if(e.getSource()==b17)
+        {
+            tf.setText("");
+        }
+    }
+ 
+    public static void main(String[] abc) 
+    {
+        calculator v = new calculator();
+    }
 }
